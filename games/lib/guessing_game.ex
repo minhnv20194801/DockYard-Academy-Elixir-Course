4e38e54f7371669e800ad6b7a12c3660630e5d00
minhnv20194801 <minhnguyenvu4@gmail.com>
2024-07-04 11:57:40 +0700

    finish Games: Guessing Game exercise

--- games/lib/guessing_game.ex
+++ games/lib/guessing_game.ex
@@ -1,21 +1,44 @@
 defmodule Games.GuessingGame do
   def play do
-    answer = Enum.random(1..10)
-    |> replay
+    Enum.random(1..10)
+    |> replay(5)
   end
 
   def replay(answer) do
-    IO.puts(answer)
     guess = String.to_integer(String.trim(IO.gets("Guess a number between 1 and 10:"), "\n"))
 
     cond do
-      guess == answer -> "You win!"
+      guess == answer ->
+        "You win!"
+
       guess > answer ->
         IO.puts("Too High!")
         replay(answer)
+
       guess < answer ->
         IO.puts("Too Low!")
         replay(answer)
+    end
+  end
+
+  def replay(answer, 0) do
+    IO.puts("You lose! the answer war #{answer}")
+  end
+
+  def replay(answer, attempt_count) do
+    guess = String.to_integer(String.trim(IO.gets("Guess a number between 1 and 10:"), "\n"))
+
+    cond do
+      guess == answer ->
+        "You win!"
+
+      guess > answer ->
+        IO.puts("Too High!")
+        replay(answer, attempt_count - 1)
+
+      guess < answer ->
+        IO.puts("Too Low!")
+        replay(answer, attempt_count - 1)
     end
   end
 end
