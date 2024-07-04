ad486cba73cf36b489b0f088ab8a1671a4920601
minhnv20194801 <minhnguyenvu4@gmail.com>
2024-07-04 11:54:25 +0700

    finish Bonus: High Or Low!

--- games/lib/guessing_game.ex
+++ games/lib/guessing_game.ex
@@ -1,13 +1,21 @@
 defmodule Games.GuessingGame do
   def play do
-    answer = Integer.to_string(Enum.random(1..10)) <> "\n"
-    guess = IO.gets("Guess a number between 1 and 10:")
+    answer = Enum.random(1..10)
+    |> replay
+  end
+
+  def replay(answer) do
+    IO.puts(answer)
+    guess = String.to_integer(String.trim(IO.gets("Guess a number between 1 and 10:"), "\n"))
 
     cond do
       guess == answer -> "You win!"
-      true ->
-        IO.puts("Incorrect!")
-        play()
+      guess > answer ->
+        IO.puts("Too High!")
+        replay(answer)
+      guess < answer ->
+        IO.puts("Too Low!")
+        replay(answer)
     end
   end
 end
