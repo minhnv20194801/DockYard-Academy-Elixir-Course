3b1766bcc04fe34b5f616af0b58896abc9329e16
minhnv20194801 <minhnguyenvu4@gmail.com>
2024-07-16 12:08:49 +0700

    Revert "finish Follow Along: LiveView Counter exercise"
    This reverts commit 444692c95ad9045d65c6b382eeafa175488e4d02.

--- live_view_counter/test/live_view_counter_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LiveViewCounterWeb.PageControllerTest do
-  use LiveViewCounterWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Counter"
-  end
-end
